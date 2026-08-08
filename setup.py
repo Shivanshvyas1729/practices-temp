1c8716ab57047101210f440779fe05b260554333
Shivanshvyas1729 <shivanshvyas1729@gmail.com>
2026-08-08 15:20:13 +0530

    project setup done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="shivansh vyas",
+    author_email="shivanshvyas29@gmail.com",
+    packages=find_packages()
+)
